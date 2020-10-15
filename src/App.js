1fc0112021c0f37bf60b0e6759f2c666f7114a2a
Brian <lawmen218@yahoo.com>
2020-10-14 23:47:33 -0400

    got grid up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+// class Grid extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         GRID
+//       </div>
+//     )
+//   }
+// }
+
+// class Main extends React.component {
+//   constructor() {
+//     super():
+//     this.state = {
+//       genertation: 0,
+//     }
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>Brian's Game of Life</h1>
+//         <Grid
+
+//         />
+//         <h2>Generations: {this.state.generation} </h2>
+//       </div>
+//     )
+//   }
+// }
+
+
+
+
+
+// function App() {
+//   return (
+
+//   );
+// }
+
+// export default App;
