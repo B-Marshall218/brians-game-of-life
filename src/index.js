4fad21d774419f9cc30c3c07b8cad79d3d9c0c1d
Brian <lawmen218@yahoo.com>
2020-10-15 23:45:14 -0400

    slow night

--- src/index.js
+++ src/index.js
@@ -21,17 +21,21 @@ class Box extends React.Component {
 
 class Grid extends React.Component {
   render() {
+    // *16 bc theres 16 pixels 
     const width = (this.props.cols * 16);
     var rowsArr = [];
 
     var boxClass = "";
+    // Nested for loop to send data to array 
     for (var i = 0; i < this.props.rows; i++) {
       for (var j = 0; j < this.props.cols; j++) {
+        // creates the box id to go with each box
         let boxId = i + "" + j;
 
-
+        // Checking to see if the box is on or off 
         boxClass = this.props.gridFull[i][j] ? "box on" : "box off"
         rowsArr.push(
+          // Pushing a bunch of boxes onto an array
           <Box
             boxClass={boxClass}
             key={boxId}
@@ -45,7 +49,7 @@ class Grid extends React.Component {
     }
     return (
       <div className="grid" style={{ width: width }}>
-        { rowsArr}
+        {rowsArr}
       </div>
     )
   }
@@ -65,10 +69,44 @@ class Main extends React.Component {
     }
   }
 
+  selectBox = (row, col) => {
+    // cant update state directly, make a copy array update copy array then 
+    // push to original array
+    let gridCopy = arrayClone(this.state.gridFull);
+    gridCopy[row][col] = !gridCopy[row][col];
+    this.setState({
+      gridFull: gridCopy
+    })
+  }
+
+  seed = () => {
+    console.log("SEED")
+    let gridCopy = arrayClone(this.state.gridFull);
+    // need to go through every square of the grid to decide if its on or off
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        // randomly chooses something. Random number between 0-4
+        if (Math.floor(Math.random() * 4) === 1) {
+          console.log("random")
+          gridCopy[i][j] = true;
+        }
+      }
+    }
+    this.setState({
+      gridFull: gridCopy
+    })
+
+  }
+
+  componentDidMount() {
+    this.seed();
+  }
+
   render() {
     return (
       <div>
         <h1>Brian's Game of Life</h1>
+        {/* passing in grids props */}
         <Grid
           gridFull={this.state.gridFull}
           rows={this.rows}
@@ -79,6 +117,12 @@ class Main extends React.Component {
       </div>
     )
   }
+}
+
+// This makes the deep clone of the array. Since its nested we need to 
+// clone all the arrays inside of arrays vs. slicing
+function arrayClone(arr) {
+  return JSON.parse(JSON.stringify(arr));
 }
 
 
